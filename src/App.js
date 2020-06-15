6c71475b6d0081ff00e4c47c0d31bae3e94cb9c9
Maralexis <malexisgonzalez10@gmail.com>
2020-06-15 17:54:29 -0500

    other commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        Weather APP
+        Weather APP.... It Works
         <Weatherlocation />
       </div>
     );
