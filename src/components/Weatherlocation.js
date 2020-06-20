68c75d4a6d7afd02dcc17736770f26388e0d1a2d
Maralexis <malexisgonzalez10@gmail.com>
2020-06-20 18:41:59 -0500

    Trtying to make a merge

--- src/components/Weatherlocation.js
+++ src/components/Weatherlocation.js
@@ -3,7 +3,7 @@ import Location from './Location'
 import WeatheData from './WeatherData'
 const WeatherLocation = () => (
     <div>
-        <Location city= {"Puente Nacional Mano"}></Location>
+        <Location city= {"Puente Nacional mano"}></Location>
         <WeatheData></WeatheData>
     </div>
 );
