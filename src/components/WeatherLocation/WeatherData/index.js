80bbe953c9e933aab0d8648c9af18232082189d6
Maralexis <malexisgonzalez10@gmail.com>
2020-06-22 15:21:43 -0500

    Destructuturin with Complex Objets

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import WeatherExtraInfo from './WeatherExtraInfo'
 import WeatherTemperature from './WeatherTeperature'
-import {
-    CLOUDY,
-} from './../../../constants/weather';
 import './styles.css'
 
-const WeatherData = () =>(
-    <div className="weatherDataCont" >
+const WeatherData = ({data : {temperature, weatherState, humidity, wind}}) =>(
+     <div className="weatherDataCont" >
         <WeatherTemperature 
-            temperature={20}
-            weatherState={CLOUDY}
+            temperature={temperature}
+            weatherState={weatherState}
         />
-        <WeatherExtraInfo humidity={70} wind={"10 km/h"}/>
+        <WeatherExtraInfo humidity={humidity} wind={wind}/>
     </div>
 );
 
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,11 +2,21 @@ import React from 'react'
 import Location from './Location'
 import WeatheData from './WeatherData'
 import './styles.css';
+import {
+    CLOUDY,
+} from './../../constants/weather';
+
+const data = {
+    temperature : 6,
+    weatherState : CLOUDY,
+    humidity : 10,
+    wind : '110 km/h', 
+}
 
 const WeatherLocation = () => (
     <div className="weatherLocationCont">
         <Location city= {"Puente Nacional Mano"}></Location>
-        <WeatheData></WeatheData>
+        <WeatheData data={data}></WeatheData>
     </div>
 );
 
