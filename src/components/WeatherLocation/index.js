788f362b05735ece689b434f1d3419d0fd612deb
Maralexis <malexisgonzalez10@gmail.com>
2020-06-22 16:15:35 -0500

    Using Constructor, state, setState, this. buttons

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -4,9 +4,17 @@ import WeatheData from './WeatherData'
 import './styles.css';
 import {
     CLOUDY,
+    WINDY,
 } from './../../constants/weather';
 
 const data = {
+    temperature : 45,
+    weatherState : WINDY,
+    humidity : 20,
+    wind : '220 km/h', 
+}
+
+const data2 = {
     temperature : 6,
     weatherState : CLOUDY,
     humidity : 10,
@@ -14,12 +22,30 @@ const data = {
 }
 
 //This is a Class Component
-class WeatherLocation extends Component{ 
+class WeatherLocation extends Component{
+
+    constructor() {
+        super();
+        this.state = {
+            city : 'Buenos Aires',
+            data : data,
+        };
+    }
+    //Inside of a Class Component, you have to use "this." to call a function
+    handleUpdateClick = () => {
+        console.log("Update Button clicked");
+        this.setState ({
+            city : 'Camboya',
+            data : data2,
+        });
+    }
   render(){
+      const { city, data } = this.state;
       return(
         <div className="weatherLocationCont">
-        <Location city= {"Puente Nacional Mano"}></Location>
+        <Location city= {city}></Location>
         <WeatheData data={data}></WeatheData>
+        <button onClick={this.handleUpdateClick}>Actualizar</button>
         </div>
       );
   }
